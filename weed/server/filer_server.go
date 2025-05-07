231b5ec07eb27c0fe4f04567202d2abfeb18a84d
bruce-zzz <bruce.zou@hhy-data.com>
2025-05-07 19:22:35 +0800

    put load configuration earlier

--- weed/server/filer_server.go
+++ weed/server/filer_server.go
@@ -151,6 +151,20 @@ func NewFilerServer(defaultMux, readonlyMux *http.ServeMux, option *FilerOption)
 	if len(option.Masters.GetInstances()) == 0 {
 		glog.Fatal("master list is required!")
 	}
+
+	if !util.LoadConfiguration("filer", false) {
+		v.SetDefault("leveldb2.enabled", true)
+		v.SetDefault("leveldb2.dir", option.DefaultLevelDbDir)
+		_, err := os.Stat(option.DefaultLevelDbDir)
+		if os.IsNotExist(err) {
+			os.MkdirAll(option.DefaultLevelDbDir, 0755)
+		}
+		glog.V(0).Infof("default to create filer store dir in %s", option.DefaultLevelDbDir)
+	} else {
+		glog.Warningf("skipping default store dir in %s", option.DefaultLevelDbDir)
+	}
+	util.LoadConfiguration("notification", false)
+
 	v.SetDefault("filer.options.max_file_name_length", 255)
 	maxFilenameLength := v.GetUint32("filer.options.max_file_name_length")
 	glog.V(0).Infof("max_file_name_length: %d", maxFilenameLength)
@@ -168,19 +182,6 @@ func NewFilerServer(defaultMux, readonlyMux *http.ServeMux, option *FilerOption)
 
 	go stats.LoopPushingMetric("filer", string(fs.option.Host), fs.metricsAddress, fs.metricsIntervalSec)
 	go fs.filer.KeepMasterClientConnected(ctx)
-
-	if !util.LoadConfiguration("filer", false) {
-		v.SetDefault("leveldb2.enabled", true)
-		v.SetDefault("leveldb2.dir", option.DefaultLevelDbDir)
-		_, err := os.Stat(option.DefaultLevelDbDir)
-		if os.IsNotExist(err) {
-			os.MkdirAll(option.DefaultLevelDbDir, 0755)
-		}
-		glog.V(0).Infof("default to create filer store dir in %s", option.DefaultLevelDbDir)
-	} else {
-		glog.Warningf("skipping default store dir in %s", option.DefaultLevelDbDir)
-	}
-	util.LoadConfiguration("notification", false)
 
 	fs.option.recursiveDelete = v.GetBool("filer.options.recursive_delete")
 	v.SetDefault("filer.options.buckets_folder", "/buckets")
