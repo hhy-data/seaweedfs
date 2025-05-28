27fb603cb30231e30c62d0adc12a3ece20bacb9c
bruce-zzz <bruce.zou@hhy-data.com>
2025-05-28 13:03:38 +0800

    rm replica check failure 503

--- weed/server/volume_server_handlers_admin.go
+++ weed/server/volume_server_handlers_admin.go
@@ -1,9 +1,11 @@
 package weed_server
 
 import (
-	"github.com/seaweedfs/seaweedfs/weed/topology"
 	"net/http"
 	"path/filepath"
+
+	"github.com/seaweedfs/seaweedfs/weed/glog"
+	"github.com/seaweedfs/seaweedfs/weed/topology"
 
 	"github.com/seaweedfs/seaweedfs/weed/pb/volume_server_pb"
 	"github.com/seaweedfs/seaweedfs/weed/stats"
@@ -20,8 +22,9 @@ func (vs *VolumeServer) healthzHandler(w http.ResponseWriter, r *http.Request) {
 		if vinfo.ReplicaPlacement.GetCopyCount() > 1 {
 			_, err := topology.GetWritableRemoteReplications(vs.store, vs.grpcDialOption, vinfo.Id, vs.GetMaster)
 			if err != nil {
-				w.WriteHeader(http.StatusServiceUnavailable)
-				return
+				glog.V(0).Infoln("volume %d has no writable remote replication: %v", vinfo.Id, err)
+				//w.WriteHeader(http.StatusServiceUnavailable)
+				//return
 			}
 		}
 	}
