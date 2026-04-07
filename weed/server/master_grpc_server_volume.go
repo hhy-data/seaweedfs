282923ea22be45f159bc0a4ba601506ba6aae764
Guang Jiong Lou <7991675+27149chen@users.noreply.github.com>
2026-04-07 17:20:59 +0800

    get topology without volumes (#42)

--- weed/server/master_grpc_server_volume.go
+++ weed/server/master_grpc_server_volume.go
@@ -216,7 +216,7 @@ func (ms *MasterServer) VolumeList(ctx context.Context, req *master_pb.VolumeLis
 	}
 
 	resp := &master_pb.VolumeListResponse{
-		TopologyInfo:      ms.Topo.ToTopologyInfo(),
+		TopologyInfo:      ms.Topo.ToTopologyInfo(req.WithoutVolumes),
 		VolumeSizeLimitMb: uint64(ms.option.VolumeSizeLimitMB),
 	}
 
